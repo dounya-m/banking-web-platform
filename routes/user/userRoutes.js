00259e4fc1c3a6c975594579302203fe5f065834
dounya-m <mellouki.dounya@gmail.com>
2023-01-17 00:22:45 +0100

    cors

--- routes/user/userRoutes.js
+++ routes/user/userRoutes.js
@@ -57,7 +57,11 @@ router.post('/login', async(req,res) => {
     }else{
         res.status(401).json("Wrong Credentials")
     }
+})
 
+router.get('/oneUser/:id', async(req, res) => {
+    const user = await User.findById(req.params.id)
+    res.status(400).json(user)
 })
 const generateToken  = (id) => {
     return jwt.sign({id}, process.env.JWT_SECRET,{
